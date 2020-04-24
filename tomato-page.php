a7dd91755e3e1ced2534aee15b7cf87346cb8e90
Austin Nguyen <astngyn@gmail.com>
2020-04-23 20:44:54 -0700

    Updated add to cart button

--- tomato-page.php
+++ tomato-page.php
@@ -193,6 +193,10 @@ form.example::after {
     <h3>Tomato</h3>
     <p class="price">$1/lb</p>
     <p><button>Add to Cart</button></p>
+    <input type="hidden" name="id" value="Tomato" />
+    <input type="hidden" name="price" value="1" />
+    <input type="hidden" name="stock" value="1" />
+    <input type="hidden" name="weight" value="1" />
   </div>
   <br>
   <br>
