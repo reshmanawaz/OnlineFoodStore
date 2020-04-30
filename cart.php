9f51964d3bbc933368f88d78d9108fdbe53a2b50
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2020-04-30 00:50:11 -0700

    edited cart.php

--- cart.php
+++ cart.php
@@ -109,6 +109,32 @@ form.example::after {
   clear: both;
   display: table;
 }
+
+
+table {
+border-collapse: collapse;
+width: 50%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>Item</th>
+<th>Quantity</th>
+<th>Price</th>
+</tr>
+
+
 </style>
 <body style="background-color:Cornsilk;">
 
@@ -155,11 +181,28 @@ form.example::after {
 
   <a href="#news">Delivery</a>
   <a href="#home">About Us</a>
-
-
 </div>
 
 
 
+<?php
+$conn = mysqli_connect("localhost", "root", "", "users");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT , product_id, product_stock, product_price FROM inventory";
+$result = $conn->query($sql);
+if($result -> num_row > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["product_id"]. "</td><td>" . $row["product_stock"] . "</td><td>"
+. $row["product_price"]. "</td></tr>";
+}
+echo "</table>";
+}
+else { echo "0 results"; }
+$conn->close();
+?>
 </body>
 </html>
